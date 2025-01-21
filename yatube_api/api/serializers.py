ba7f8eb53596191ead247cdf49f69293692fbc2f
Anton Fayfer <fayfer2004@mail.ru>
2025-01-21 18:11:08 +0600

    modify 20 files and 1 add

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,47 @@
+from wsgiref.validate import validator
+
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
+from rest_framework.validators import UniqueValidator
 
 from posts.models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = serializers.SlugRelatedField(read_only=True, slug_field="username")
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field="username", default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+    )
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Follow.objects.all(), fields=["user", "following"]
+        )
+    ]
+
+    def validate(self, data):
+        if self.context["request"].user != data["author"]:
+            return data
+        raise serializers.ValidationError("На тебя нельзя подписаться")
+
+    class Meta:
+        fields = "__all__"
+        model = Follow
