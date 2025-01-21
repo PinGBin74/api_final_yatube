73d2366155f6e2ee84bfa6e34a099e06abc92ed3
Anton Fayfer <fayfer2004@mail.ru>
2025-01-21 18:30:37 +0600

    pep8 fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,53 +1,52 @@
-
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueTogetherValidator
 
-from posts.models import Comment, Post, Follow
+from posts.models import Comment, Post, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field="username",
-                              read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
-        fields = "__all__"
+        fields = '__all__'
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True,
-                                          slug_field="username")
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
 
     class Meta:
-        fields = "__all__"
+        fields = '__all__'
         model = Comment
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field="username",
-        default=serializers.
-        CurrentUserDefault()
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
     )
     following = serializers.SlugRelatedField(
-        slug_field="username",
-        queryset=User.objects.all(),
+        slug_field='username',
+        queryset=User.objects.all()
     )
-    validators = [
-        UniqueTogetherValidator(
-            queryset=Follow.objects.all(),
-            fields=["user", "following"]
-        )
-    ]
+
+    validators = [UniqueTogetherValidator(
+        queryset=Follow.objects.all(),
+        fields=['user', 'following'])]
 
     def validate(self, data):
-        if (self.context["request"].
-                user != data["author"]):
+        if self.context['request'].user != data.get('following'):
             return data
-        raise (serializers.
-               ValidationError("На тебя нельзя подписаться"))
+        raise serializers.ValidationError(
+            'Нельзя подписаться на себя'
+        )
 
     class Meta:
-        fields = "__all__"
+        fields = ('__all__')
         model = Follow
